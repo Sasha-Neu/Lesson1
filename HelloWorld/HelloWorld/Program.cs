e47211f32653436229bdc1422156da1f9a2ba157
Sasha <machete.pills@gmail.com>
2023-01-26 01:31:01 -0800

    First code

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+
+            name = EnterName();
+
+            var greeting = "Hello";
+           
+
+            Console.WriteLine($"{ greeting }, { name }!");
+        }
+
+        /// <summary>
+        /// Entering name from console
+        /// </summary>
+        /// <returns>Username</returns>
+        static string EnterName()
+        {
+            Console.WriteLine("Please, enter your name:");
+
+            string userName;
+            userName = Console.ReadLine();
+
+            return userName;
         }
     }
 }
